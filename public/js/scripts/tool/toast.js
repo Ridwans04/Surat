533ca789297e2092e2e9956d224d830a45a41139
Ridwans04 <projectridwan04@gmail.com>
2024-05-08 07:54:33 +0700

    upd

---
+++ public/js/scripts/tool/toast.js
@@ -0,0 +1,11 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    iconColor: 'white',
+    customClass: {
+        popup: 'colored-toast',
+    },
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+})
